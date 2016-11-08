a3763bbbb0decc3ab5712536b40866504980bdd9
91073657 <91073657@mvusd.net>
2016-11-08 09:26:02 -0800

    DigiFlower

--- section04mastery/DigiFlower.java
+++ section04mastery/DigiFlower.java
@@ -12,16 +12,26 @@ public class DigiFlower
   {
     Tortoise.show();
     Tortoise.setSpeed(10);
-    //    Make the background silver  --#8
-    Tortoise.getBackgroundWindow().setColor();
-    color = PenColors.Grays.Silver(color);
-    //    Make the line the tortoise draws 3 pixels wide --#15
-    //    createColorPalette (recipe below) --#9.1
-    //        Do the following 15 times --#13.1
-    //          drawOctogon (recipe below) --#10.1
-    //          Turn the tortoise 1/15th of 360 degrees to the right --#12
-    //        Repeat --#14.2
-    //    ------------- Recipe for createColorPalette --#9.2
+    Tortoise.getBackgroundWindow().setColor(PenColors.Grays.Silver);
+    Tortoise.setPenWidth(3);
+    for (int i = 0; i < 15; i++)
+    {
+      Tortoise.turn(360 / 15);
+      createColorPalette();
+      drawOctogon();
+    }
+  }
+  private static void drawOctogon()
+  {
+    for (int i = 0; i < 8; i++)
+    {
+      Tortoise.setPenColor(ColorWheel.getNextColor());
+      Tortoise.move(50);
+      Tortoise.turn(360 / 8);
+    }
+  }
+  private static void createColorPalette()
+  {
     Color color1 = PenColors.Reds.Red;
     Color color2 = PenColors.Oranges.DarkOrange;
     Color color3 = PenColors.Yellows.Gold;
@@ -34,15 +44,5 @@ public class DigiFlower
     ColorWheel.addColor(color3);
     ColorWheel.addColor(color2);
     ColorWheel.addColor(color1);
-    //    ------------- End of createColorPalette recipe --#9.3
-    //
-    //    ------------- Recipe for drawOctogon --#10.2
-    for (int i = 0; i < 8; i++)
-    {
-      Tortoise.setPenColor(ColorWheel.getNextColor());
-      Tortoise.move(50);
-      Tortoise.turn(360 / 8);
-    }
-    //    ------------- End of drawOctogon recipe --#10.3
   }
 }
